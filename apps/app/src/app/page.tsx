ed936e5fb879cd2ad3c3ad5a78deea6a3349fb05
Sebastien Dubois <seb@dsebastien.net>
2024-08-16 18:38:34 +0200

    style(all): improved mobile display

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -76,49 +76,53 @@ export default function Index() {
   };
 
   return (
-    <div>
-      <div className="">
-        <h1 className="p-6 bg-pink-600 text-white text-3xl">&nbsp;dSebastien Article Image Generator</h1>
+    <div className="w-full">
+      <h1 className="p-6 bg-pink-600 text-white text-3xl">&nbsp;dSebastien Article Image Generator</h1>
 
-        <form onSubmit={generatePrediction} className="flex flex-col items-center w-full">
-          <div className="mt-4 flex flex-col lg:flex-row">
-            <div className="pl-4 pt-8 pr-4 pb-4 gap-8 flex flex-col lg:flex-row lg:gap-4 lg:p-4">
-              <FloatLabel>
-                <Password id="api-key" value={apiKey} feedback={false} minLength={1} onChange={(e) => setApiKey(e.target.value)} />
-                <label htmlFor="api-key">API Key</label>
-              </FloatLabel>
-              <FloatLabel>
-                <InputText id="prompt" value={prompt} className="w-[64rem]" minLength={1} onChange={(e) => setPrompt(e.target.value)} />
-                <label htmlFor="prompt">Prompt</label>
-              </FloatLabel>
-            </div>
-            <div className="p-4">
-              <Button type="submit" label="Generate" icon="pi pi-check" iconPos="right" loading={loading}></Button>
-            </div>
+      <form onSubmit={generatePrediction} className="flex flex-col items-center w-full">
+        <div className="mt-4 flex flex-col lg:flex-row w-full">
+          <div className="pl-4 pt-8 pr-4 pb-4 gap-8 flex flex-col lg:flex-row lg:gap-4 lg:p-4">
+            <FloatLabel>
+              <Password id="api-key" value={apiKey} feedback={false} minLength={1} onChange={(e) => setApiKey(e.target.value)} />
+              <label htmlFor="api-key">API Key</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText
+                id="prompt"
+                value={prompt}
+                className="w-full lg:w-[32rem] xl:w-[64rem]"
+                minLength={1}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+              <label htmlFor="prompt">Prompt</label>
+            </FloatLabel>
           </div>
-        </form>
-        <div>
-          {/* TODO Switch to Prime react Toast: https://primereact.org/toast/ */}
-          {error ? <div className="mt-4 p-4 bg-red-500">{error}</div> : ''}
+          <div className="p-4">
+            <Button type="submit" label="Generate" icon="pi pi-check" iconPos="right" loading={loading}></Button>
+          </div>
+        </div>
+      </form>
+      <div>
+        {/* TODO Switch to Prime react Toast: https://primereact.org/toast/ */}
+        {error ? <div className="mt-4 p-4 bg-red-500">{error}</div> : ''}
 
-          <div className="mt-4 p-4">
-            <div className="flex flex-col items-center justify-center w-full">
-              {!loading && prediction && prediction.output && (
-                <Image
-                  src={Array.isArray(prediction.output) ? prediction.output[prediction.output.length - 1] : prediction.output}
-                  width={500}
-                  height={500}
-                  alt="Image generated using AI"
-                  className="object-cover rounded-md border-gray-300 border-2"
-                />
-              )}
+        <div className="mt-4 p-4">
+          <div className="flex flex-col items-center justify-center w-full">
+            {!loading && prediction && prediction.output && (
+              <Image
+                src={Array.isArray(prediction.output) ? prediction.output[prediction.output.length - 1] : prediction.output}
+                width={500}
+                height={500}
+                alt="Image generated using AI"
+                className="object-cover rounded-md border-gray-300 border-2"
+              />
+            )}
 
-              {loading && (
-                <div className="w-[500px] h-[500px] flex flex-col items-center justify-center object-center">
-                  <ProgressSpinner className="w-32 h-32" strokeWidth={4} aria-label="Loading" />
-                </div>
-              )}
-            </div>
+            {loading && (
+              <div className="w-[500px] h-[500px] flex flex-col items-center justify-center object-center">
+                <ProgressSpinner className="w-32 h-32" strokeWidth={4} aria-label="Loading" />
+              </div>
+            )}
           </div>
         </div>
       </div>
